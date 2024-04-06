7fecb3cf9a5b36736b0def1c30f9ab99b71332bd
SARB <mrsinghbusiness05@gmail.com>
2024-04-06 20:26:01 +0530

    login user working

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -8,24 +8,20 @@ export const SignupUser = async(req,res) => {
         console.log('signup data =',username , firstname ,lastname , password);
 
         if(!username || !firstname || !lastname || !password){
-             return res.status(400).json({
+             return res.status(404).json({
                  message : "Enter All Details"
              })
         }
 
         const bcryptpass = await bcrypt.hash(password,10);
 
-        
         const  dbuser = await User.create({
             username,
             firstname,
             lastname,
             password : bcryptpass
         });
-        
-        const token = await jwt.sign({_id : dbuser._id},process.env.JWT_SECRET);
-        console.log('token gen =',token);
-        
+
         const user = await dbuser.save();
 
         return res.status(200).json({
@@ -41,7 +37,48 @@ export const SignupUser = async(req,res) => {
 export const LoginUser = async(req,res) => {
     try {
         
+        const { username , password } = req.body;
+
+        if(!username || !password){
+            return res.status(404).json({
+                message : "Enter All Details",
+            })
+        }
+
+        const findUser = await User.findOne({username});
+
+        if(!findUser){
+            return res.status(404).json({
+                message : "User not Found"
+            })
+        }
+
+        const decryptpass = await bcrypt.compare(password ,findUser.password);
+        console.log('decrypt pass =',decryptpass);
+
+        if(!decryptpass){   
+            return res.status(404).json({
+                message : "Password Incorrect"
+            })  
+        }
+        
+        const token = await jwt.sign({_id : findUser._id},process.env.JWT_SECRET);
+        console.log('token gen =',token);
+        
+        return res.status(200).json({
+            message : "User logged in",
+            token,
+        })
+
     } catch (error) {
         console.log('error =',error);
+    }
+}
+
+export const authMiddleware = async(req,res) => {
+    try {
+        
+    } catch (error) {
+            console.log('error =',error) ;
     }
 }
