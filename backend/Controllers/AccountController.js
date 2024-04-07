e2fda61592061f6dc29e706a706ed4b78f8bc518
SARB <mrsinghbusiness05@gmail.com>
2024-04-07 14:02:13 +0530

    balance  left - working

--- backend/Controllers/AccountController.js
+++ backend/Controllers/AccountController.js
@@ -1,9 +1,14 @@
 
 import Account from "../Schemas/BankSchema.js";
+import User from "../Schemas/UserSchemas.js";
 
 export const balance = async(req,res) => {
     try {   
-        const account = await Account.findOne(req.userid);
+
+        const account = await Account.findOne({
+            userid : req.userid
+        });
+        console.log('account for bal=',account);
 
         return res.status(200).json({
             balance : account.balance,
