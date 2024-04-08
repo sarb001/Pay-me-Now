fffd0d244a6ff3064d74fa27cc7e41c408aeb995
SARB <mrsinghbusiness05@gmail.com>
2024-04-09 04:14:22 +0530

    sender verified - successfully

--- frontend/src/components/SendMoney.jsx
+++ frontend/src/components/SendMoney.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams ,useLocation } from 'react-router-dom';
 
 const SendMoney = () => {
+
+  const [searchparams] = useSearchParams();
+  const id = searchparams.get("id");
+  console.log('id is =',id);
+  const name = searchparams.get("name");
+  console.log('name is =',name);
+
 
    const handlemoney = () => {
        
@@ -12,6 +19,7 @@ const SendMoney = () => {
        <div style = {{margin:'%',display:'flex' ,flexDirection: 'column' ,justifyContent:'center' , alignItems:'center' , backgroundColor:'lightgrey' , padding:'3% 0%'}}>
 
           <span style = {{margin :'3%'}}>
+             <h2> Payment to = {name}  </h2>
           <label> Enter Amount (in Rs.) </label>
           <input style  = {{padding:'1%'}} type = "number"  placeholder='Enter Amount '  />
           </span>
