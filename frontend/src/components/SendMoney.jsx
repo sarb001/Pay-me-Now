cf6bc4139a0c2d5a45dc041e94d2295ed760f80e
SARB <mrsinghbusiness05@gmail.com>
2024-04-09 03:54:45 +0530

    adding qury in url

--- frontend/src/components/SendMoney.jsx
+++ frontend/src/components/SendMoney.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 const SendMoney = () => {
 
    const handlemoney = () => {
-      
+       
    }
 
   return (
       <>
        <div style = {{margin:'%',display:'flex' ,flexDirection: 'column' ,justifyContent:'center' , alignItems:'center' , backgroundColor:'lightgrey' , padding:'3% 0%'}}>
 
-        <span style = {{margin :'3%'}}>
-         <label> Enter Amount (in Rs.) </label>
-         <input style  = {{padding:'1%'}} type = "number"  placeholder='Enter Amount '  />
-        </span>
+          <span style = {{margin :'3%'}}>
+          <label> Enter Amount (in Rs.) </label>
+          <input style  = {{padding:'1%'}} type = "number"  placeholder='Enter Amount '  />
+          </span>
 
         <div>
          <button onClick={handlemoney} style = {{padding:'1%'}}> Send Money </button>
--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link  ,useNavigate,useSearchParams } from 'react-router-dom';
 import axios from 'axios' ;
 
 const Users = () => {
     
+   const [searchuser,setsearchuser] = useState('');
    const [users,setusers] = useState('');
    const token   = localStorage.getItem('token');
    console.log('token users ==',token);
+
+   const navigate = useNavigate();
 
    useEffect(() => {
       const fetchdata = async () => {
@@ -22,38 +25,45 @@ const Users = () => {
           console.log('error =',error);
         } 
       }
-    fetchdata();
-      },[])
+     fetchdata();
+    },[])
 
 
   return (
     <>
       <div style = {{margin:'4%',display:'flex' ,flexDirection: 'column' ,justifyContent:'center' , alignItems:'center'}}>
-        <div>
-          <input style = {{padding:'2% 2%'}} type = "text"  placeholder= "Enter User Name"  value ={users} 
-          onChange={(e) => setusers(e.target.value)} />
-         </div>
-        <div>
-          <h2> Showing All Users </h2>
-        </div>
 
-       <div style = {{ display:'flex' , flexDirection:'column' ,backgroundColor:'lightsalmon',margin:'3%',padding:'4%'}}>
-         
-          {users && users?.map((i) => 
-          {
-            return (
-              <div key = {i._id}>
-               <div style = {{paddingTop:'8% 5%'}}>
-                  {i.username} 
-                  <span>  <Link to = "/send"> Send Money </Link>  </span>
-               </div>
-              </div>
-            )
-          })}
+          <div>
+            <input style = {{padding:'2% 2%'}} type = "text"  placeholder= "Enter User Name"  value ={searchuser} 
+            onChange={(e) => setsearchuser(e.target.value)} />
+          </div>
 
-          
-         
-       </div>
+          <div>
+            <h2> Showing All Users </h2>
+          </div>
+
+          <div style = {{ display:'flex' , flexDirection:'column' ,backgroundColor:'lightsalmon',margin:'1%',padding:'2%'}}>
+            
+              {users && users?.map((i) => 
+              {
+                return (
+                  <div key = {i._id}>
+                  <div style = {{paddingTop:'2% 1%'}}>
+                      {i.username} 
+                      <span> 
+                        <button style = {{padding:'2%'}} 
+                        onClick={(e) =>{
+                          navigate("/send?id=" + i._id + "&name=" + i.username)
+                        }}> 
+                         <Link to = "/send"> Send Money </Link> 
+                        </button>
+                       </span>
+                  </div>
+                  </div>
+                )
+              })}
+
+          </div>
 
        </div>
     </>
