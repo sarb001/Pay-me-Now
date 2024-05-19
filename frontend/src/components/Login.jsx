c97ec05ff7612c33b31474351583f17818ecce79
SARB <mrsinghbusiness05@gmail.com>
2024-05-19 11:11:58 +0530

    credentials added - login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -21,7 +21,8 @@ const Login = () => {
    }
 
    const addCredentials = () => {
-      
+      setusername('amandeep@22');
+      setpassword('amandeep@22')
    }
 
   return (
@@ -46,7 +47,7 @@ const Login = () => {
             <div style = {{margin:'3%',display:'grid',gridTemplateRows : '1fr 1fr' , justifyContent:'space-around'}}>
               <button className='bg-black text-white px-5 py-2' type = "submit"> Login Now </button>
               <br />
-              <button  onClick={addCredentials} style = {{padding:'1% 2%'}} > Guest Credentials </button>
+              <button className='bg-black text-white px-5 py-2'  onClick={addCredentials} style = {{padding:'1% 2%'}} > Guest Credentials </button>
             </div>
             <div style = {{paddingTop:'3%'}}>
                 <Link  to = "/signup" > Don't have an Account? Create Now </Link>
