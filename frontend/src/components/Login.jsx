d587571083b251a0b02fe55fdf4565cf9a7adc8d
SARB <mrsinghbusiness05@gmail.com>
2024-07-04 16:11:29 +0530

    form restructured

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -39,39 +39,41 @@ const Login = () => {
 
           <div className='py-4 px-8 bg-zinc-400 flex flex-col gap-4 '>
 
-                <div>
-                  <label className='flex flex-col'> 
-                    <div className='font-semibold'> Username </div>
-                    <div>
-                      <input className='p-0 w-full' type = "text"  placeholder='Enter User Name..'  
-                      value = {username}  onChange={(e) => setusername(e.target.value)}  />
-                    </div>
-                </label>
-                </div>
-
-                <div>
-                  <label className='flex flex-col' >
-                    <div className='font-semibold'> Password </div>
-                    <div>
-                      <input className='p-0 w-full' type = "text"  placeholder='Enter Password...'  
-                      value = {password}  onChange={(e) => setpassword(e.target.value)}  />
-                    </div>
+                  <div>
+                    <label className='flex flex-col'> 
+                      <div className='font-semibold'> Username </div>
+                      <div>
+                        <input className='p-0 w-full' type = "text"  placeholder='Enter User Name..'  
+                        value = {username}  onChange={(e) => setusername(e.target.value)}  />
+                      </div>
                   </label>
-                </div>
+                  </div>
+
+                  <div>
+                    <label className='flex flex-col' >
+                      <div className='font-semibold'> Password </div>
+                      <div>
+                        <input className='p-0 w-full' type = "text"  placeholder='Enter Password...'  
+                        value = {password}  onChange={(e) => setpassword(e.target.value)}  />
+                      </div>
+                    </label>
+                  </div>
           
                 <div className='flex flex-col justify-center gap-4'>
 
+                    <div>
+                      <button className='bg-black  w-full text-white px-5 py-2' type = "submit">
+                        {loading ? "Logging In.." : " Login Now " } 
+                      </button>
+                    </div>
 
-                  <div>
-                    <button className='bg-black  w-full text-white px-5 py-2' type = "submit">
-                      {loading ? "Logging In.." : " Login Now " } 
-                    </button>
                 </div>
+          </div>
+        </form>
 
                   <div>
                     <button className='bg-black w-full text-white px-5 py-2'  
                     onClick={addCredentials} > Guest Credentials </button>
-                  </div>
                   </div>
 
                 <div>
@@ -80,8 +82,8 @@ const Login = () => {
 
           </div>
 
-       </form>
-       </div>
+       
+      
     </>
   )
 }
